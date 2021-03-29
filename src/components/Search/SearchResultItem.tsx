31d66b01303933d31944a307aef81cfb0f653e4b
Vali <1a@users.noreply.github.com>
2021-03-29 16:33:34 +0100

    search result base styling

--- src/components/Search/SearchResultItem.tsx
+++ src/components/Search/SearchResultItem.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "./SearchResultItem.scss";
 
 export type PersonType = "Housing Officer" | string;
 
@@ -39,22 +40,34 @@ export interface ISearchResult {
 }
 
 export function SearchResultItem(p: ISearchResult) {
+    const bemBlock = "mtfh-search-result";
+    // bem element notation utility
+    const __ = (suffix) => `${bemBlock}__${suffix}`;
     const isMultipleTenancies = p.tenures.length > 1;
+    const address = "1 Hill Street, E5 9HL";
     return (
-        <div className="mtfh-search__result">
-            <p>
+        <div className={bemBlock}>
+            <p className={__("title")}>
                 {p.title} {p.firstname}
                 {p.middleName ? ` ${p.middleName}` : ""} {p.surname}
             </p>
-            {isMultipleTenancies && <p> Multiple tenancies </p>}
-            <p>DOB: {p.dateOfBirth}</p>
+            <p className={__("subtitle")}>{address}</p>
+            <p className={__("row")}>
+                <strong>DOB:</strong> {p.dateOfBirth}
+            </p>
+            {isMultipleTenancies && (
+                <div className={__("row")}> Multiple tenancies </div>
+            )}
             {!isMultipleTenancies && (
-                <div>
+                <div className={__("row")}>
                     {p.tenures.map((t, index) => (
                         <p key={index}>{t.assetFullAddress}</p>
                     ))}
                 </div>
             )}
+            <div className={__("row")}>
+                <a href="#">More details</a>
+            </div>
         </div>
     );
 }
