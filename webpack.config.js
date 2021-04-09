b655afe5e9a8a33f5f21ec2e2bb22c32d0965522
Vali <1a@users.noreply.github.com>
2021-04-09 13:19:02 +0100

    config to load svgs

--- webpack.config.js
+++ webpack.config.js
@@ -2,29 +2,40 @@ const { merge } = require("webpack-merge");
 const singleSpaDefaults = require("webpack-config-single-spa-react-ts");
 
 module.exports = (webpackConfigEnv, argv) => {
-  const defaultConfig = singleSpaDefaults({
-    orgName: "mtfh",
-    projectName: "search",
-    webpackConfigEnv,
-    argv,
-  });
+    const defaultConfig = singleSpaDefaults({
+        orgName: "mtfh",
+        projectName: "search",
+        webpackConfigEnv,
+        argv,
+    });
 
-  return merge(defaultConfig, {
-    // modify the webpack config however you'd like to by adding to this object
-    module: {
-      rules: [
-        {
-          test: /\.s[ac]ss$/i,
-          use: [
-            // Creates `style` nodes from JS strings
-            "style-loader",
-            // Translates CSS into CommonJS
-            "css-loader",
-            // Compiles Sass to CSS
-            "sass-loader",
-          ],
+    return merge(defaultConfig, {
+        // modify the webpack config however you'd like to by adding to this object
+        module: {
+            rules: [
+                {
+                    test: /\.s[ac]ss$/i,
+                    use: [
+                        // Creates `style` nodes from JS strings
+                        "style-loader",
+                        // Translates CSS into CommonJS
+                        "css-loader",
+                        // Compiles Sass to CSS
+                        "sass-loader",
+                    ],
+                },
+                {
+                    test: /\.svg$/,
+                    use: [
+                        {
+                            loader: "svg-url-loader",
+                            options: {
+                                limit: 10000,
+                            },
+                        },
+                    ],
+                },
+            ],
         },
-      ],
-    },
-  });
+    });
 };
