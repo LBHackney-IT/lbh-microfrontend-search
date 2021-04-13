392ff83ae85575ed4aaceb5e2330c11e210700c8
Vali <1a@users.noreply.github.com>
2021-04-13 12:36:45 +0100

    removed explicit typing

--- src/services/Search.ts
+++ src/services/Search.ts
@@ -4,6 +4,6 @@ const headers = {
     "Content-Type": "application/json",
     "x-api-key": "PdVeW0KpKr4o1vPDhlWwK6fK7QfM3v2h7FAwD5ml",
 };
-export function findByPersonName(name: string = "") {
+export function findByPersonName(name = "") {
     return fetch(buildSearchUrl(name), { headers });
 }
