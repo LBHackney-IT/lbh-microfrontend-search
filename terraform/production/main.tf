24907c82189e3804466c15c70d1a1ee4a7f03a6b
George Wilde <georgewilde@users.noreply.github.com>
2021-04-15 09:44:00 +0100

    chore(repo): add AWS region to terraform config (#5)
    Add AWS region to the terraform config as this is required to deploy the infrastructure.

--- terraform/production/main.tf
+++ terraform/production/main.tf
@@ -1,10 +1,13 @@
 resource "aws_s3_bucket" "frontend-bucket-production" {
   bucket = "lbh-housing-tl-search-frontend-production.hackney.gov.uk"
+  region = "eu-west-2"
   acl    = "public-read"
+
   versioning {
     enabled = true
   }
-    website {
+
+  website {
     index_document = "index.html"
     error_document = "error.html"
   }
